2511a922598e2a8aab0348b212a3acd7396d9ffd
shanda-matc <shanda@madisoncollege.edu>
2023-12-12 00:46:47 -0600

    made changes in User entity

---
+++ src/main/java/edu/shanda/entity/User.java
@@ -0,0 +1,97 @@
+package edu.shanda.entity;
+
+import javax.persistence.*;
+
+/**
+ * The type User.
+ */
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param username    the username
+     * @param email       the email
+     */
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    /**
+     * Gets user id.
+     *
+     * @return the user id
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * Sets user id.
+     *
+     * @param userId the user id
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets username.
+     *
+     * @param username the username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Gets email.
+     *
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets email.
+     *
+     * @param email the email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
+
+
+
+
+
+
