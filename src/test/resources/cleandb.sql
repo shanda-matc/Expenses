8e211b1411718eff0933358439c29faa6a4357d3
shanda-matc <shanda@madisoncollege.edu>
2023-12-12 00:38:48 -0600

    added cleandb.sql

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,3 +1,6 @@
-delete from users;
-INSERT INTO users VALUES (1, "Shilpa", "Handa", "shanda",'shanda@madisoncollege.edu', 'secret123', 'shilpa', 'usr1.jpg', 'bio for user one'),(2, "Robin", "Parker", "Rparker",'rparker@gmail.com', 'secret123', 'parker', 'usr2.jpg', 'bio for user two'),
-        (3, "Arjun", "Kumar","Akumar", 'kumar@yahoo.in', 'secret123', 'arjun', 'kumar.jpg', 'my name is arjun');
+SET foreign_key_checks = 0;
+DELETE FROM user;
+DELETE FROM stories;
+DELETE FROM comments;
+DELETE FROM categories;
+SET foreign_key_checks = 1;
